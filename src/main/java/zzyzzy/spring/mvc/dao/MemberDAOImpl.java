8c5646270c29f1657275eb502b1c38724ae68a30
10playgrounds <10playgrounds@naver.com>
2022-09-30 12:42:14 +0900

    simpleJdbcInsert 적용

--- src/main/java/zzyzzy/spring/mvc/dao/MemberDAOImpl.java
+++ src/main/java/zzyzzy/spring/mvc/dao/MemberDAOImpl.java
@@ -1,7 +1,12 @@
 package zzyzzy.spring.mvc.dao;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import zzyzzy.spring.mvc.vo.MemberVO;
@@ -9,20 +14,22 @@ import zzyzzy.spring.mvc.vo.MemberVO;
 @Repository("mdao")
 public class MemberDAOImpl implements MemberDAO {
 
-	@Autowired
+	//@Autowired
 	private JdbcTemplate jdbcTemplate;
+	private SimpleJdbcInsert simpleJdbcInsert;
+	
+	public MemberDAOImpl(DataSource dataSource) {
+		simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+		.withTableName("member")
+		.usingColumns("userid", "passwd", "name", "email");
+	}
 	
 	@Override
 	public int insertMember(MemberVO mvo) {
-		String sql = " insert into member "
- 		  + " (userid, passwd, name, email) "
- 		  + " values (?, ?, ?, ?) ";
+		SqlParameterSource params = 
+				new BeanPropertySqlParameterSource(mvo);
 		
-		Object[] params = new Object[] {
-			mvo.getUserid(), mvo.getPasswd(),
-			mvo.getName(), mvo.getEmail() };
-				
-		return jdbcTemplate.update(sql, params);
+		return simpleJdbcInsert.execute(params);
 	}
 
 }
