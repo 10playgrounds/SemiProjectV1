d95800a6f7cbc27875c8d2d1ccd63a574f8f61e0
10playgrounds <10playgrounds@naver.com>
2022-09-30 15:40:43 +0900

    게시판 목록보기

--- src/main/java/zzyzzy/spring/mvc/dao/BoardDAO.java
+++ src/main/java/zzyzzy/spring/mvc/dao/BoardDAO.java
@@ -1,9 +1,13 @@
 package zzyzzy.spring.mvc.dao;
 
+import java.util.List;
+
 import zzyzzy.spring.mvc.vo.BoardVO;
 
 public interface BoardDAO {
 
 	int insertBoard(BoardVO bvo);
+
+	List<BoardVO> selectBoard();
 
 }
--- src/main/java/zzyzzy/spring/mvc/service/BoardService.java
+++ src/main/java/zzyzzy/spring/mvc/service/BoardService.java
@@ -1,9 +1,17 @@
 package zzyzzy.spring.mvc.service;
 
+import java.util.List;
+
 import zzyzzy.spring.mvc.vo.BoardVO;
 
 public interface BoardService {
 
 	boolean newBoard(BoardVO bvo);
 
+	List<BoardVO> readBoard();
+
 }
+
+
+
+
