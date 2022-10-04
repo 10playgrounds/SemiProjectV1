916a0c6190a37a35d9a92025f06fc13f81187312
10playgrounds <10playgrounds@naver.com>
2022-10-04 14:52:42 +0900

    세션을 이용한 새글쓰기

--- src/main/java/zzyzzy/spring/mvc/controller/BoardController.java
+++ src/main/java/zzyzzy/spring/mvc/controller/BoardController.java
@@ -1,5 +1,7 @@
 package zzyzzy.spring.mvc.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,9 +33,16 @@ public class BoardController {
 		return "board/view";
 	}
 	
+	// 로그인 안했다면 -> redirect:/login
+	// 로그인 했다면 -> board/write
 	@GetMapping("/write")
-	public String write() {
-		return "board/write";
+	public String write(HttpSession sess) {
+		String returnPage = "board/write";
+		
+		if (sess.getAttribute("m") == null) 
+			returnPage = "redirect:/login";
+		
+		return returnPage;
 	}
 	
 	@PostMapping("/write")
