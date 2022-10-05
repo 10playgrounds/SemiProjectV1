7bbda5cbb5bccffbf2b364d2c2948ecfb2f53862
10playgrounds <10playgrounds@naver.com>
2022-10-05 15:37:19 +0900

    게시판 검색기능 구현 1

--- src/main/java/zzyzzy/spring/mvc/service/BoardServiceImpl.java
+++ src/main/java/zzyzzy/spring/mvc/service/BoardServiceImpl.java
@@ -24,9 +24,9 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
-	public List<BoardVO> readBoard(int snum) {
+	public List<BoardVO> readBoard(String fkey, String fval, int snum) {
 		
-		return bdao.selectBoard(snum);
+		return bdao.selectBoard(fkey, fval, snum);
 	}
 
 	@Override
@@ -36,9 +36,9 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
-	public int readCountBoard() {
+	public int readCountBoard(String fkey, String fval) {
 		
-		return bdao.selectCountBoard();
+		return bdao.selectCountBoard(fkey, fval);
 	}
 
 }
